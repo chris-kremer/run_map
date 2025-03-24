451a6e464d0cd657655e6071314cd1a94c1a2867
Christian <christian.kremer@whu.edu>
2025-03-24 13:58:45 +0100

    Initial Commit

---
+++ Run_Map/Run_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Run_MapApp.swift
+//  Run_Map
+//
+//  Created by Christian Kremer on 24.03.25.
+//
+
+import SwiftUI
+
+@main
+struct Run_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Run_MapTests/Run_MapTests.swift
@@ -0,0 +1,17 @@
+//
+//  Run_MapTests.swift
+//  Run_MapTests
+//
+//  Created by Christian Kremer on 24.03.25.
+//
+
+import Testing
+@testable import Run_Map
+
+struct Run_MapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
